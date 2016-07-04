c5112b810c2c5d13a78535e3c27130491ff474b6
Berdnikov Alexey <aberdnikov@gmail.com>
2016-07-04 17:26:51 +0500

    Update init.php

--- init.php
+++ init.php
@@ -1,5 +1,5 @@
 <?php
 \Larakit\StaticFiles\Manager::package('larakit/sf-angular-ckeditor')
-                            ->usePackage('larakit/sf-ckeditor')
                             ->ngModule('ngCkeditor')
+                            ->usePackage('larakit/sf-ckeditor')
                             ->js('//cdnjs.cloudflare.com/ajax/libs/ng-ckeditor/0.2.1/ng-ckeditor.min.js');
