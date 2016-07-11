054047650633bc7f6ca20fa4c145b0ec1d556bff
Alexey Berdnikov <aberdnikov@gmail.com>
2016-07-11 21:41:53 +0500

    fixed

--- init.php
+++ init.php
@@ -1,5 +1,8 @@
 <?php
 \Larakit\StaticFiles\Manager::package('larakit/sf-angular-ckeditor')
-                            ->ngModule('ngCkeditor')
-                            ->usePackage('larakit/sf-ckeditor')
-                            ->js('//cdnjs.cloudflare.com/ajax/libs/ng-ckeditor/0.2.1/ng-ckeditor.min.js');
+    ->ngModule('ngCkeditor')
+    ->usePackage('larakit/sf-ckeditor')
+    ->usePackage('larakit/ng-larakit')
+    ->setSourceDir('public')
+    ->jsPackage('ng-ckeditor.min.js')
+    ->cssPackage('ng-ckeditor.css');
