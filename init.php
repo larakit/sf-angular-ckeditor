e26140543b67409f821b63c582193226197b253c
Alexey Berdnikov <aberdnikov@gmail.com>
2018-02-19 04:07:01 +0500

    fixed

--- init.php
+++ init.php
@@ -1,6 +1,7 @@
 <?php
 \Larakit\StaticFiles\Manager::package('larakit/sf-angular-ckeditor')
     ->usePackage('larakit/sf-ckeditor')
+    ->usePackage('larakit/sf-angular')
     ->setSourceDir('public')
     ->jsPackage('angular-ckeditor.js');
 \Larakit\LkNgModule::register('ckeditor');
